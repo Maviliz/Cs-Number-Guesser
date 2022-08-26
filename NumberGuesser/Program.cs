9c6dec4be6928ea259181eda8ef2da7348a16d01
aavilmari@gmail.com <$0l0Pr0gram>
2022-08-25 20:06:38 -0500

    Added verification for user answer (no non-numbers and length must be exactly 4). Finished compareNumber. Working MVP

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -8,6 +8,16 @@ string answer = "";
 
 string answerMaker() { return Convert.ToString(rnd.Next(1, 7)); }
 
+bool IsAllDigits(string uG)
+{
+    foreach (char c in uG)
+    {
+        if (!char.IsDigit(c))
+            return false;
+    }
+    return true;
+}
+
 for (int i =0; i<4; i++)
 {
     answer += answerMaker();
@@ -16,18 +26,32 @@ int attemptsTaken = 0;
 int maxAttempts = 10;
 string userGuess;
 
-Console.WriteLine(answer);
 
- string takeAGuess () { 
+//Console.WriteLine(answer);
+
+ string TakeAGuess () { 
     Console.Write("Please enter a 4-digit number. All digits must be between the numbers 1-6: ");
     userGuess = Console.ReadLine();
-    Console.WriteLine(userGuess + " is the user's guess");
+ 
+    while (userGuess.Length != 4)
+    {
+        Console.WriteLine("Your guess must be 4 digits long. Please enter a 4-digit number made up of numbers between 1-6: ");
+        userGuess = Console.ReadLine();
+        
+    }
+    while (IsAllDigits(userGuess) == false)
+    {
+        Console.WriteLine("Your guess must be 4 digits, each a number between 1-6: ");
+        userGuess = Console.ReadLine();
+    }
+
     attemptsTaken++;
     return userGuess;
 }
 
  void statusCheck(string result)
 {
+    
     if (result.Equals("++++"))
     {
         Console.WriteLine("Congratulations! You have correctly guessed the code and won the game!");
@@ -38,46 +62,51 @@ Console.WriteLine(answer);
         if (attemptsTaken < maxAttempts)
         {
             int attemptsLeft = maxAttempts - attemptsTaken;
-            Console.WriteLine("Try again! You have " + attemptsLeft + " remaining attempt(s).");
+            Console.WriteLine("\nTry again! You have " + attemptsLeft + " remaining attempt(s).");
+        } else
+        {
+            Console.WriteLine("Max of 10 attempts reached. Game over :(");
         }
-        Console.WriteLine(answerMaker + " is the code to guess");
     }
 }
 
-//string compareNumber(string answer)
-//{
-//    //Done: write function to compare numbers: loop through each digit, return -,+, or " " depending on comparison result
-//    //String comparison for '-' check was not working, switched to char for easier comparison
-//    string result = "";
-//    for (int i = 0; i < answer.Length(); i++)
-//    {
-//        char current = userGuess.charAt(i);
-//        if (number.charAt(i) == answer.charAt(i))
-//        {
-//            result = result + "+";
-//        }
-//        else
-//        {
-//            boolean contains = false;
-//            for (int j = 0; j < answer.Length(); j++)
-//            {
-//                if (current == answer.charAt(j))
-//                {
-//                    result = result + "-";
-//                    contains = true;
-//                    break;
-//                    //fixed: fix this logic because it's still printing " " for all other digits of target
-//                }
-//            }
-//            if (!contains)
-//            {
-//                result = result + " ";
-//            }
-//        }
+string compareNumber(string answer, string userGuess)
+{
+    string result = "";
+    for (int i = 0; i < answer.Length; i++)
+    {
+        char current = userGuess[i];
+        if (userGuess[i] == answer[i])
+        {
+            result = result + "+";
+        }
+        else
+        {
+            bool contains = false;
+            for (int j = 0; j < answer.Length; j++)
+            {
+                if (current == answer[j])
+                {
+                    result = result + "-";
+                    contains = true;
+                    break;
+                    
+                }
+            }
+            if (!contains)
+            {
+                result = result + " ";
+            }
+        }
 
-//    }
-//    Console.WriteLine(result);
-//    return result;
-//}
+    }
+    Console.WriteLine(result);
+    return result;
+}
 
-TakeAGuess();
+statusCheck(compareNumber(answer, TakeAGuess()));
+
+while (attemptsTaken < maxAttempts)
+{
+    statusCheck(compareNumber(answer, TakeAGuess()));
+}
